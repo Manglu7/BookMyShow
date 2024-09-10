3d206f4d9d9fabb00d6781f7972de55457223251
Ajay Sharma <ajaysharma502074@gmail.com>
2024-09-10 08:41:54 +0530

    final

--- bms/tests.py
+++ bms/tests.py
@@ -1,3 +1,4 @@
+from datetime import timedelta, datetime
 from unittest.mock import Mock
 
 from django.test import TestCase, TransactionTestCase
@@ -7,6 +8,7 @@ from bms.models import User, Movie, Region, Theater, Screen, Seat, SeatType, Sho
     ShowSeatType
 from bms.services import BookShowService
 from bms.views import BookingViewSet
+from django.contrib.auth.hashers import make_password
 
 
 # Create your tests here.
@@ -15,38 +17,39 @@ class BookingTestCase(TransactionTestCase):
 
     def setUp(self):
         self.user, created = User.objects.get_or_create(
-            id=1,
-            email='abc@gmail.com',
-            password='password'
+            email='abc@gmail.com'
         )
+        if created:
+            self.user.password = make_password('password')
+            self.user.save()
 
         self.movie, created = Movie.objects.get_or_create(
-            id=1,
+
             title='asd',
             release_date='2024-07-27',
             runtime=200
         )
 
         self.region, created = Region.objects.get_or_create(
-            id=1,
+
             name='Delhi'
         )
 
         self.theater, created = Theater.objects.get_or_create(
-            id=1,
+
             name='inox',
             region=self.region
         )
 
         self.screen, created = Screen.objects.get_or_create(
-            id=1,
+
             region=self.region,
             name='audi1',
             theater=self.theater
         )
 
         self.seat1, created = Seat.objects.get_or_create(
-            id=1,
+
             row_number=1,
             col_number=1,
             number="11",
@@ -55,7 +58,7 @@ class BookingTestCase(TransactionTestCase):
         )
 
         self.seat2, created = Seat.objects.get_or_create(
-            id=2,
+
             row_number=1,
             col_number=2,
             number="12",
@@ -64,35 +67,39 @@ class BookingTestCase(TransactionTestCase):
         )
 
         self.feature, created = Feature.objects.get_or_create(
-            id=1,
+
             name='2D'
         )
 
+        self.naive_datetime = datetime(2024, 9, 10, 7, 54, 0)
+
+        self.aware_datetime = timezone.make_aware(self.naive_datetime, timezone.get_current_timezone())
+
         self.show, created = Show.objects.get_or_create(
-            id=1,
+
             movie=self.movie,
-            start_time=timezone.now(),
-            end_time=timezone.now() + timezone.timedelta(hours=2),
+            start_time=self.aware_datetime,
+            end_time=self.aware_datetime + timedelta(hours=2),
             screen=self.screen,
             features=self.feature
         )
 
         self.show_seat1, created = ShowSeat.objects.get_or_create(
-            id=1,
+
             show=self.show,
             seat=self.seat1,
             show_seat_status=ShowSeatStatus.AVAILABLE
         )
 
         self.show_seat2, created = ShowSeat.objects.get_or_create(
-            id=2,
+
             show=self.show,
             seat=self.seat2,
             show_seat_status=ShowSeatStatus.AVAILABLE
         )
 
         self.show_seat_type, created = ShowSeatType.objects.get_or_create(
-            id=1,
+
             show=self.show,
             seat_type=SeatType.PLATINUM,
             price=250
